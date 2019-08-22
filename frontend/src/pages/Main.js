6e2a8c8543e5254f60d345038ab01eb3ff8e30c0
unknown <Eugnio Oliveira>
2019-08-22 20:17:12 -0300

    criando a interface

---
+++ frontend/src/pages/Main.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+import './Main.css';
+
+import api from '../services/api';
+
+export default function Main({ match }){
+
+    const [users, setUsers] = useState([]); 
+
+    useEffect(() => {
+        async function loadUsers(){
+            const response = await api.get('/devs/', {
+                headers: { 
+                    user: match.params.id 
+                }
+            });
+            setUsers(response.data);
+        } 
+        loadUsers();
+    }, [match.params.id]);
+
+    return(
+        <div className="main-container">
+            <img src={logo} alt="tinDEV" />
+            <ul>
+                {users.map(user => (
+                    <li key={user._id}>
+                    <img src={user.avatar} alt={user.name} />
+                    <footer>
+                        <strong>{user.name}</strong>
+                        <p>{user.bio}</p>
+                    </footer>
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="Dislike" />
+                        </button>
+                        <button type="button">
+                        <img src={like} alt="Like" />
+                        </button>
+                    </div>
+                </li>
+                ))}
+                
+
+            </ul>
+        </div>
+        );
+}
